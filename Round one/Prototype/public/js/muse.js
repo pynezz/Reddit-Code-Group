72ee2aa804adbcad6a940149d0c8ad26f9830d67
Kevin <kevin.ka_96@hotmail.com>
2021-07-03 12:18:05 +0200

    added some comments

--- Round one/Prototype/public/js/muse.js
+++ Round one/Prototype/public/js/muse.js
@@ -1,20 +1,26 @@
 const API_URL = "https://api.datamuse.com/words?";
 //const resultList = document.querySelector('.result-list');
 
+//* Get the HTML tags that we'll attach the returned API data to:
 const nounResultList = document.querySelector(".result-list-nouns");
 const rhymesResultList = document.querySelector(".result-list-rhymes");
 const similarResultList = document.querySelector(".result-list-similar");
+
+//* Get the other neccessary HTML tags:
 const inputField = document.querySelector(".input-field");
 const searchButton = document.querySelector(".search-word");
-searchButton.addEventListener("click", afterClick);
+
+//* Add listeners to wait for events 
+searchButton.addEventListener("click", afterClick);    
 inputField.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    runAPI();
-    return false;
+  if (event.keyCode === 13) {   // Keycode 13 = Enter key
+    runAPI();                   // If enter key is pressed call the API
+    return false;               // Preventing accidental multiple calls 
   }
 });
 
 /*
+            ? |---  DOCUMENTATION  ---| ?
 Params:
     ml = similar words
     rel_[code] = related word
@@ -41,42 +47,51 @@ Related word codes: (ex: rel_jja)
     rhy = Rhymes    
 */
 
+//* The parameters to tell the API what data we want
+// It's not in use for the moment. 
+// I placed them in <input value=""> instead.
+// From there it's easy to access it from JS with checkBox.value 
 var Choices = {
   SIMILAR: "ml",
-  POPULAR_NOUNS: "jja",
-  POPULAR_ADJ: "jjb",
-  SYNONYMS: "syn",
-  ANTONYMS: "ant",
-  KINDOF: "spc",
-  GENERAL: "gen",
-  COMPRISED: "com",
-  RHYMES: "rhy",
+  POPULAR_NOUNS: "rel_jja",
+  POPULAR_ADJ: "rel_jjb",
+  SYNONYMS: "rel_syn",
+  ANTONYMS: "rel_ant",
+  KINDOF: "rel_spc",
+  GENERAL: "rel_gen",
+  COMPRISED: "rel_com",
+  RHYMES: "rel_rhy",
 };
 
 // Checkboxes that we need to check if is checked
+// 
 const nounsCheckBox = document.getElementById("noun");
 const similarCheckBox = document.getElementById("similar");
 const rhymesCheckBox = document.getElementById("rhymes");
 
-// This is an array of bools
+// This is an array of (not bools) checkBoxElements. 
+// We access these to check if they're checked or not
 let checks = [nounsCheckBox, similarCheckBox, rhymesCheckBox];
 
 function useChecks(search) {
-  checks.forEach((element) => {
-    // If an element is checked (true)
-    if (element.checked) {
-      // ...then create a request for that checkbox
-      let res = "no results";
-      console.log(element);
-      let req = new XMLHttpRequest();
-      req.open("GET", `${API_URL}${element.value}=${search}&max=12`, search);
-      req.setRequestHeader("Accept", "application/json");
-      req.send();
-      req.onload = () => {
-        if (req.status == 200) {
-          res = req.response;
+  checks.forEach((element) => {          // For every element in 'checks'               
+    if (element.checked) {               // If an element is checked (true)
+      let res = "no results";            // Create a string variable with default value "no results" 
+      //console.log(element);            // Checking if it works
+      let req = new XMLHttpRequest();    // Make a new request:
+      req.open(
+          "GET", 
+          `${API_URL}${element.value}=${search}&max=12`,
+          search);
+      req.setRequestHeader(                 
+          "Accept",
+          "application/json");              
+      req.send();                        // Send the request to the API with the checkbox's value as a parameter
+      req.onload = () => {               // When the request is done
+        if (req.status == 200) {         // If the status is 200 (no problems)
+          res = req.response;            // Assign the response to the string variable 'res' 
         }
-        generateHTML(res, element.value); // And generate the corresponding html (element.value is the parameter)
+        generateHTML(res, element.value);// And lastly, generate the HTML
       };
     }
   });
